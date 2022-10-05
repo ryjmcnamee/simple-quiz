9bffb09da46209ccb57065d5727ab1c8c0eb2b60
ryjmcnamee <ryjmcnamee@gmail.com>
2022-10-05 14:30:02 -0700

    add more js comments, fix score initialization

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,13 +50,17 @@ retryButton.addEventListener("click", startQuizOver)
 //---------------------------//
 //Display Question
 function displayQuestion(questionIndex) {
-
+    //Populate question text element
     questionText.textContent = quizQuestions[questionIndex].question;
 
+    //Check if previous score exists -> initializes to 0
+    if(questionIndex == 0){localStorage.setItem('simple-quiz.score',0)};
+
+    //Initialize answer list to empty if existing
     if (answerList.childElementCount > 0) {
         answerList.innerHTML = "";
     }
-
+    //Iterate over answers and create buttons from object data
     for (var i = 0; i < quizQuestions[questionIndex].answers.length; i++) {
         var answerButton = document.createElement("button");
 
@@ -72,8 +76,10 @@ function displayQuestion(questionIndex) {
 //Submit Answer
 
 function submitAnswer(questionIndex, chosenAnswerIndex) {
-
+    //If correct
     if (quizQuestions[questionIndex].answers[chosenAnswerIndex].correct) {
+        //If score exists -> add +1
+        //Else -> set to 1
         if (questionIndex != 0) {
             localStorage.setItem("simple-quiz.score", (parseInt(localStorage.getItem("simple-quiz.score")) + 1))
         }
@@ -91,32 +97,94 @@ function submitAnswer(questionIndex, chosenAnswerIndex) {
 
 //---------------------------//
 //Display Score
-
 function displayScore() {
-    var score = document.getElementById("score");
-
+    //Change displayed screen
     questionBox.setAttribute("style", "display:none;");
     scoreScreen.setAttribute("style", "display:block;");
 
+    //Locals
+    var score = localStorage.getItem("simple-quiz.score");
+    var scoreContainer = document.createElement("div");
+
+    var highScoreSubmissionForm = document.createElement("form");
+    var highScoreSubmissionInstructionsSpan = document.createElement("span");
+    var highScoreSubmissionInstructions = "Please enter your name below to save your highscore -";
+    var highScoreNameInput = document.createElement("input");
+    var highScoreSubmitBtn = document.createElement("button");
+
+
+    //Initialize score message
+    var scoreMessage =
+        "YOU GOT" + "\n"
+        + score + "\n"
+        + "POINTS";
+
+    //Initialize form elements
+    highScoreNameInput.setAttribute('type', 'text');
+    highScoreNameInput.setAttribute('placeholder', 'e.g Jimmy Proton');
+
+    highScoreSubmitBtn.setAttribute('type', 'button');
+    highScoreSubmitBtn.textContent = 'Submit';
+
+
+
+    //Construct form
+    highScoreSubmissionForm.appendChild(highScoreSubmissionInstructionsSpan);
+    highScoreSubmissionInstructionsSpan.textContent = highScoreSubmissionInstructions;
+    highScoreSubmissionForm.appendChild(highScoreNameInput);
+    highScoreSubmissionForm.appendChild(highScoreSubmitBtn);
+
+    //Submit score
+    function scoreSubmit() {
+        recordScore(score, highScoreNameInput.value);
+        displayHighScores();
+    }
+
+    //Initialize Screen
+    scoreScreen.innerHTML = '';
+
+    //Construct screen
+    scoreContainer.textContent = scoreMessage;
+    scoreScreen.appendChild(scoreContainer);
+    scoreScreen.appendChild(highScoreSubmissionForm);
+
+    //Event Listener for form
+    highScoreSubmitBtn.addEventListener('click', scoreSubmit);
 
 };
 
 //---------------------------//
 //Record Score
-
 function recordScore(earnedScore, enteredName) {
+    //Initialize array variable to be later copied
     var scoreArray = JSON.parse(localStorage.getItem("simple-quiz.highScores"))
-    if (scoreArray == null) { scoreArray = [] };
-    var recordedScore = { score: earnedScore, name: enteredName }
-    if (scoreArray.length > 0) {
-        var scoreArrayLength = scoreArray.length
-        for (var i = 0; i < scoreArrayLength; i++) {
 
-            if (earnedScore >= scoreArray[i].score) { scoreArray.splice(i, 0, recordedScore); break; }
-            else{scoreArray.push(recordedScore);break;};
+    //If scoreArray is null, no high scores exist -> scoreArray to be initalized as array
+    if (scoreArray == null) { scoreArray = [] };
+
+    //Construct object
+    var recordedScore = { score: earnedScore, name: enteredName }
+    
+    //Check if array has entries -> check score against entries to determine sort order
+    if (scoreArray.length > 0) {
+        
+        //Locals
+        const scoreArrayLength = scoreArray.length
+        var isInserted = false;
+
+        //Check against each current member
+        for (var i = 0; i < scoreArrayLength; i++) {
+            //If higher or equal to, insert and break
+            if (earnedScore >= scoreArray[i].score) 
+            { scoreArray.splice(i, 0, recordedScore); isInserted = true; break; }
         }
-    }
-    else { scoreArray.push(recordedScore); console.log("empty") }
+
+        //If no insertion, append to end
+        if(!isInserted){scoreArray.push(recordedScore);}
+        }
+    else {scoreArray.push(recordedScore);}
+
+    //Send first N scores to local storage
     localStorage.setItem("simple-quiz.highScores", JSON.stringify(scoreArray.slice(0, numOfHighScores)));
 }
 
@@ -177,4 +245,9 @@ function startQuizOver() {
 
 //DEBUG 
 displayQuestion(0);
-recordScore(18, "TODO FIXME!");
+recordScore(18, "Entry 2");
+recordScore(16, "Entry 4");
+recordScore(18, "Entry 1");
+recordScore(17, "Entry 3");
+recordScore(12, "Not Visible");
+recordScore(12, "Entry 5");

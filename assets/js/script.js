b45bce22423a1b2cd303dfced2a97642c36300ea
ryjmcnamee <ryjmcnamee@gmail.com>
2022-10-04 23:03:32 -0700

    add answer population

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,59 @@
 //Initialize Quiz Object
 var quizQuestions = [
-    {question:"Question 1",
-        answer:"Answer 1"},
-    {question:"Question 2",
-        answer:"Answer 2"},
-    {question:"Question 3",
-        answer:"Answer 3"},
-    {question:"Question 4",
-        answer:"Answer 4"},
+    {
+        question: "Question 1",
+        answers: [
+            { text: "Answer 1", correct: true },
+            { text: "Answer 2", correct: false },
+            { text: "Answer 3", correct: false },
+        ]
+    },
+    {
+        question: "Question 1",
+        answers: [
+            { text: "Answer 1", correct: true },
+            { text: "Answer 2", correct: false },
+            { text: "Answer 3", correct: false },
+        ]
+    },
+    {
+        question: "Question 1",
+        answers: [
+            { text: "Answer 1", correct: true },
+            { text: "Answer 2", correct: false },
+            { text: "Answer 3", correct: false },
+        ]
+    },
+
 ]
 //---------------------------//
 //Initialize Event Listeners
 
-//TODO: Currently these are placeholders
 var questionBox = document.getElementById("questionBox");
-var answerSelect = document.getElementById("answerSelect");
+var answerList = document.getElementById("answerList");
 var submitButton = document.getElementById("submitButton");
 
-//PLACEHOLDER OVERRIDES
-questionBox = {value:""};
-answerSelect = {value:""};
-submitButton = {value:""};
 
 //---------------------------//
 //Display Question
 
+function displayQuestion(questionIndex) {
 
+    questionBox.textContent = quizQuestions[questionIndex].question;
+    for(var i = 0; i < quizQuestions[questionIndex].answers.length;i++){
+        var answerButton = document.createElement("button");
+
+        answerButton.setAttribute("value",i);
+        answerButton.setAttribute("onclick","submitAnswer()");
+        answerButton.innerHTML = quizQuestions[questionIndex].answers[i].text;
+        console.log(answerButton);
+        answerList.appendChild(answerButton);
+    }
+}
+
+//---------------------------//
+//Advance Question
+
+
+//DEBUG 
+displayQuestion(1);

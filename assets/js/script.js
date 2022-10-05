742e946be002f0773c82a9b147a8544cca61077f
ryjmcnamee <ryjmcnamee@gmail.com>
2022-10-04 22:08:56 -0700

    add basic js for outline begin

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//Initialize Quiz Object
+var quizQuestions = [
+    {question:"Question 1",
+        answer:"Answer 1"},
+    {question:"Question 2",
+        answer:"Answer 2"},
+    {question:"Question 3",
+        answer:"Answer 3"},
+    {question:"Question 4",
+        answer:"Answer 4"},
+]
+//---------------------------//
+//Initialize Event Listeners
+
+//TODO: Currently these are placeholders
+var questionBox = document.getElementById("questionBox");
+var answerSelect = document.getElementById("answerSelect");
+var submitButton = document.getElementById("submitButton");
+
+//PLACEHOLDER OVERRIDES
+questionBox = {value:""};
+answerSelect = {value:""};
+submitButton = {value:""};
+
+//---------------------------//
+//Display Question
+
+
